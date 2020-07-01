eb98b6357d5574c21e065c19761650022b7b28e3
Pranay Pratyush <pranay.pratyush@gmail.com>
2020-07-01 23:38:53 +0530

    https://github.com/DefinitelyTyped/DefinitelyTyped/issues/31245

--- twin.d.ts
+++ twin.d.ts
@@ -1,6 +1,7 @@
 // twin.d.ts
 import 'twin.macro'
 import styledComponent, { css as cssProperty } from 'styled-components'
+import {} from 'styled-components/cssprop'
 
 declare module 'twin.macro' {
     const css: typeof cssProperty
