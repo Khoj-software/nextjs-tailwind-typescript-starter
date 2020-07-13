f5f73374be8172ae1973a6f9f14070e1c5817924
Pranay Pratyush <pranay.pratyush@gmail.com>
2020-07-13 21:47:31 +0530

    Bug fix

--- pages/jsx.tsx
+++ pages/jsx.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import tw from 'twin.macro'
+const CustomBtn = tw.button`text-white font-bold py-2 px-4 rounded`
 export default function jsxExample(): React.FunctionComponentElement<void> {
-    const CustomBtn = tw.button`text-white font-bold py-2 px-4 rounded`
     return (
         <section>
             <CustomBtn tw="bg-red-500 ">Button 1</CustomBtn>
